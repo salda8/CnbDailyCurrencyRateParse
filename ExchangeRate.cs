88e186acfc71217cf576f2576d51c2bca903dd78
salda8 <saldicek@gmail.com>
2018-05-17 00:04:45 +0200

    InitialCommit

---
+++ ExchangeRate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CurrencyRate
+{
+    public struct ExchangeRate
+    {
+        public string Zeme { get; set; }
+        public string Mena { get; set; }
+        public int Mnozstvi { get; set; }
+        public string Kod { get; set; }
+        public decimal Kurz { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is ExchangeRate))
+                return false;
+
+            var rate = (ExchangeRate)obj;
+            // compare elements here
+            return Kod == rate.Kod;
+
+        }
+
+    }
+}
---
+++ ExchangedResult.cs
@@ -0,0 +1,31 @@
+﻿namespace CurrencyRate
+{
+    public class ExchangedResult
+    {
+        public ExchangedResult()
+        {
+        }
+
+        public decimal Castka
+        {
+            get
+            {
+                return 0m;
+            }
+            set
+            {
+            }
+        }
+
+        public string Mena
+        {
+            get
+            {
+                return string.Empty;
+            }
+            set
+            {
+            }
+        }
+    }
+}
